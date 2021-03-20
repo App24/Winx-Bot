3ea770e2d42bdf7f70e643d2aa73509afd15e21b
App24 <kado.fofinho2@gmail.com>
2021-03-20 15:52:48 +0000

    Added env.d.ts

---
+++ src/types/env.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS{
+    export interface ProcessEnv{
+        TOKEN : string,
+        PREFIX : string,
+        OWNER_ID : string,
+        CREATORS : string,
+        SUGGESTION_CHANNEL : string
+    }
+}
